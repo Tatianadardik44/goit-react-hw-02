50e5325b600315f4d3f968fdbfc9f08110ee4e6a
Tatianadardik44 <dardiktatiana@gmail.com>
2024-05-18 23:55:12 +0200

    addLocalstorige

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 import Options from './Options'
 import Feedback from './Feedback'
 import Notification from './Notification';
 
-
+  const createLocalState = (key) => {
+    const localState = localStorage.getItem(key);
+    return localState ? JSON.parse(localState) : 0
+  }
 function App() {
   const [state, setState] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  });
+     good: createLocalState('good'),
+     neutral: createLocalState('neutral'),
+     bad: createLocalState('bad')
+  })
+  useEffect(() => {
+   localStorage.setItem('good', JSON.stringify(state.good));
+    localStorage.setItem('neutral', JSON.stringify(state.neutral));
+    localStorage.setItem('bad', JSON.stringify(state.bad));
+  }, [state])
+  
   const updateFeedback = (feedbackType) => {
     setState({...state, [feedbackType]: state[feedbackType] + 1})
     
@@ -20,14 +29,14 @@ function App() {
     setState({ good: 0, neutral: 0, bad: 0 });
   };
   const totalFeedback = state.good + state.neutral + state.bad;
-  const positive = Math.round((state.good / totalFeedback) * 100);
+  const positiveFeedback  = Math.round((state.good / totalFeedback) * 100);
   return (
     <>
       <div>
         <h1>Sip Happens Café</h1>
         <p>Please leave your feedback about our service by selecting one of the options below.</p>
         <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} hendleReset={hendleReset}  /> 
-        {totalFeedback ? <Feedback state={state} totalFeedback={  totalFeedback } positive={positive }/>:<Notification/>}
+        {totalFeedback ? <Feedback state={state} totalFeedback={  totalFeedback } positiveFeedback ={positiveFeedback  }/>:<Notification/>}
       </div>  
     
     </>
--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,11 +1,11 @@
-const Feedback = ({state,  totalFeedback, positive}) => {
+const Feedback = ({state,  totalFeedback, positiveFeedback }) => {
     return (
         <ul>
             <li>Good: {state.good }</li>
             <li>Neutral: {state.neutral}</li>
             <li>Bad: {state.bad}</li>
             <li>Total:{totalFeedback}</li>
-            <li>Positive:{positive}%</li>
+            <li>Positive:{positiveFeedback }%</li>
         </ul>
     )
 }
